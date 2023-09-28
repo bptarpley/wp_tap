c4f5835ba6b561dedeb6ef2249be38fdb8dae1f8
Bryan Tarpley <bptarpley@tamu.edu>
2023-09-28 14:39:59 -0500

    implementing new about and contact headers

--- js/tap.js
+++ js/tap.js
@@ -190,19 +190,19 @@ class HeaderImage {
                 alt: "The Texas Art Project"
             },
             '/about/': {
-                src: `${this.tap.plugin_url}/img/about-heading.png`,
-                alt: "I hope to leave behind me a collection of indigenous paintings which will faithfully portray Texas as it is. - Buck Schiwetz"
+                src: `${this.tap.plugin_url}/img/about-us.png`,
+                alt: "About Us"
             },
             '/contact/': {
-                src: `${this.tap.plugin_url}/img/about-heading.png`,
-                alt: "I hope to leave behind me a collection of indigenous paintings which will faithfully portray Texas as it is. - Buck Schiwetz"
+                src: `${this.tap.plugin_url}/img/contact-us.png`,
+                alt: "Contact Us"
             },
             '/schiwetz/gallery/': {
                 src: `${this.tap.plugin_url}/img/home-collage.png`,
                 alt: "The Buck Schiwetz Gallery"
             },
             '/schiwetz/essay/': {
-                src: `${this.tap.plugin_url}/img/about-heading.png`,
+                src: `${this.tap.plugin_url}/img/schiwetz-essay.png`,
                 alt: "I hope to leave behind me a collection of indigenous paintings which will faithfully portray Texas as it is. - Buck Schiwetz"
             },
             '/schiwetz/map/': {
