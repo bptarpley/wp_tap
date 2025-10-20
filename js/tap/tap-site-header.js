62943d6b53fb6816de59b3c06353a08fffbb89c6
Bryan Tarpley <bptarpley@gmail.com>
2025-10-20 14:58:31 -0500

    adding a divider for the DWG menu section

--- js/tap/tap-site-header.js
+++ js/tap/tap-site-header.js
@@ -30,6 +30,7 @@ export class SiteHeader {
                       <a class="dropdown-item" href="/schiwetz/home/">Gallery</a>
                       <a class="dropdown-item" href="/schiwetz/map/">Map and Timeline</a>
                       
+                      <hr class="dropdown-divider">
                       <b>Dallas Women's Gallery</b>
                       <hr class="dropdown-divider">
                       <a class="dropdown-item" href="/dwg">Home</a>
